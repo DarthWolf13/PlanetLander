0a2fb7cc273cfbb4747e4a317eb5aec08f8490d8
Bas Pijls <b.pijls@hva.nl>
2017-06-20 12:02:52 +0200

    Step 0: Adds start project

---
+++ PlanetLander/PlanetLander/PlanetLander.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace PlanetLander
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class PlanetLander : GameEnvironment
+    {
+              
+        public PlanetLander()
+        {         
+            Content.RootDirectory = "Content";
+        }
+     
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+            screen.X = 800;
+            screen.Y = 600;
+            SetFullScreen(false);
+
+            // TODO: use this.Content to load your game content here
+
+        }      
+    }
+}
