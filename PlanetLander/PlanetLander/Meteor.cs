014d76fc4d9d2f736277d53df1214db1d0dabce7
DarthWolf13 <martijn.vander.meer@quicknet.nl>
2017-06-22 13:34:11 +0200

    Step 3

---
+++ PlanetLander/PlanetLander/Meteor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PlanetLander
+{
+    class Meteor : SpriteGameObject
+    {
+        Vector2 randomPos, randomVel;
+
+        public Meteor() : base("spr_meteor")
+        {
+            randomPos = new Vector2(GameEnvironment.Random.Next(0, PlanetLander.Screen.X), GameEnvironment.Random.Next(-500, -100));
+            this.position = randomPos;
+            randomVel = new Vector2(0, GameEnvironment.Random.Next(50, 150));
+            this.velocity = randomVel;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if(this.position.Y > PlanetLander.Screen.Y)
+            {
+                this.position = randomPos;
+            }
+        }
+    }
+}
--- PlanetLander/PlanetLander/PlayingState.cs
+++ PlanetLander/PlanetLander/PlayingState.cs
@@ -9,15 +9,23 @@ namespace PlanetLander
     {
         Mountains mountains;
         Lander lander;
+        GameObjectList meteors;
 
         public PlayingState()
         {
             mountains = new Mountains();
             lander = new Lander();
+            meteors = new GameObjectList();
 
             this.Add(new SpriteGameObject("spr_background"));
             this.Add(mountains);
             this.Add(lander);
+            this.Add(meteors);
+
+            for(int i = 0; i < 5; i++)
+            {
+                this.meteors.Add(new Meteor());
+            }
         }
     }
 }
